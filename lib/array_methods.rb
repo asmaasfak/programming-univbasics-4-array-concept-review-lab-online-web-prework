64d68ea69b346fbf2b9a3bfdd1fde5bc7ff03ac0
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-04 00:04:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,16 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  idx = 0
+  idx = array.length - 1
+  
   
 end
 
 def find_max_value(array)
   # Add your solution here
+  p array.max
 end
 
 def find_min_value(array)
   # Add your solution here
+  p array.min
 end
